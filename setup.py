012fae81c702d8c397ab52df66e969bfb8400d1f
Adaptee <adaptee@gmail.com>
2010-11-20 17:27:45 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+
+from distutils.core import setup
+
+setup(  name="pinyin-comp",
+        version="0.1",
+        description="complete path containing Chinese by pinyin acronym",
+        packages=['pinyin'],
+        scripts=['pinyin-comp'] ,
+        author="Jekyll Wu",
+        author_email="adaptee@gmail.com",
+        url="http://www.github.com/adaptee/pinyin-comp",
+     )
