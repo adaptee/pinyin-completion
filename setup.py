4d659128a8d2a135f18124240bab05dd7f499c91
Adaptee <adaptee@gmail.com>
2010-11-20 17:30:37 +0800

    minor update

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ from distutils.core import setup
 setup(  name="pinyin-comp",
         version="0.1",
         description="complete path containing Chinese by pinyin acronym",
-        packages=['pinyin'],
-        scripts=['pinyin-comp'] ,
         author="Jekyll Wu",
         author_email="adaptee@gmail.com",
         url="http://www.github.com/adaptee/pinyin-comp",
+        packages=['pinyin'],
+        scripts=['pinyin-comp'] ,
      )
