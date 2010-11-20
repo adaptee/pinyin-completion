9751731b726eb46d8f383d3e50c862141d3542d3
Adaptee <adaptee@gmail.com>
2010-11-20 17:43:19 +0800

    add completion setup for bash and zsh

---
+++ shell/pinyin-comp.zsh
@@ -0,0 +1,21 @@
+
+# use pinyin-comp to perform completion based upon pinyin acronym
+function _pinyin()
+{
+    # chsdir print one candidate per line
+    # this looks weird, bug IFS='\n' does not work in interactive shell
+    local IFS=$'\n'
+
+    reply=($(pinyin-comp 0 $*))
+}
+
+# pinyin-comp is performed as one part of user-expand
+zstyle ':completion:*' user-expand _pinyin
+
+# omit origianl when showing the resul of user-expand
+zstyle ':completion:*:user-expand:*' tag-order '!original'
+
+# make use-expand perform as last, when needed
+zstyle ':completion:*' completer _oldlist _expand _force_rehash _complete _match _user_expand
+
+# vim:set ft=zsh :
